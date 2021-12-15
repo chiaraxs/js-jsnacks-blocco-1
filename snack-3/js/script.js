a44bf7a92bb3d9bb82f72ddd1ce6e42b2be0f0f3
chiaraxs <chiaracardinale92@gmail.com>
2021-12-15 17:55:40 +0100

    snack-3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,17 +1,21 @@
-// snack-3 
+// Snack-3 
 // Stampa le potenze di 2 fino a 1000.
 
 
 // ordine di costruzione:
-// Fammi un for che stampa i valori da 1 a 10 compresi
-// Ora invece di stampare i, stampami il valore di i al cubo (i**3 o Math.pow(i,3))
-// Ora invece di andare da 1 a 10 vai da 1 al numero che ha chiesto l'utente 
+// Fammi un ciclo while che mi stampa i primi 10 cubi per i numeri da 1 a 10
+// Adesso cambiamo la condizione: fammi un ciclo while che mi stampa finché il valore del cubo è minore di 1000 
 
+let i = 0;
+let y = 2
 
-for (let i = 2; i <= 1000; i++) { 
-    console.log(Math.pow(i, 2)); 
+// while (i < 10) {
+//     console.log(Math.pow(i,3));
+//     i++;
+// }
+
+while (y < 1000) {
+    y = Math.pow(i, 3);
+    console.log(y);
+    i++;
 }
-
-// stampare le potenze di 2 FINO A QUANDO non superano il valore 1000
-// 16:57
-// non stamparne 1000 
