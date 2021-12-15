049d346b54c414320b0c0a8d53ff605330a35a37
chiaraxs <chiaracardinale92@gmail.com>
2021-12-15 17:20:26 +0100

    snack-2

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,3 +1,22 @@
 // Snack-2.
 // Stampa il cubo di ciascuno dei primi N numeri, dove N è un numero indicato dall’utente. 
-// (se inserisce 10,  si stampa il cubo di 1, di 2, di 3,…)
+// (se inserisce 10, si stampa il cubo di 1, di 2, di 3,…)
+
+// ordine di costruzione:
+// Fammi un for che stampa i valori da 1 a 10 compresi
+// Ora invece di stampare i, stampami il valore di i al cubo (i**3 o Math.pow(i,3))
+// Ora invece di andare da 1 a 10 vai da 1 al numero che ha chiesto l'utente
+
+
+let userNumber = parseInt(prompt('Inserisci un numero'));
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     console.log(Math.pow(i,3));
+//     console.log(userNumber);
+// }
+
+for (let i = 1; i <= userNumber; i++) {
+    console.log(Math.pow(i,3));
+}
+
--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,2 +1,17 @@
 // snack-3 
 // Stampa le potenze di 2 fino a 1000.
+
+
+// ordine di costruzione:
+// Fammi un for che stampa i valori da 1 a 10 compresi
+// Ora invece di stampare i, stampami il valore di i al cubo (i**3 o Math.pow(i,3))
+// Ora invece di andare da 1 a 10 vai da 1 al numero che ha chiesto l'utente 
+
+
+for (let i = 2; i <= 1000; i++) { 
+    console.log(Math.pow(i, 2)); 
+}
+
+// stampare le potenze di 2 FINO A QUANDO non superano il valore 1000
+// 16:57
+// non stamparne 1000 
