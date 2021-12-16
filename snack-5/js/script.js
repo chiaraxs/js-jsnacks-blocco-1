738b9f8da1170f7bd49e3dd7efdc2b20af72871c
chiaraxs <chiaracardinale92@gmail.com>
2021-12-16 10:43:11 +0100

    snack-5

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,3 +1,22 @@
-// snack-5 
+// Snack-5 
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
 // Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+
+// creo array vuoto
+let numberList = [];
+// fisso variabile somma = 0
+let sum = 0;
+
+// creo ciclo e aggiungo prompt con richiesta numero da inserire all'utente
+// sommo i numeri aggiunti dall'utente. Il ciclo continua finchè la somma dei numeri è < 50
+do {
+    let number = parseInt(prompt('Inserisci un numero'));
+    numberList.push(number);
+    sum += number;
+    console.log(number);
+    console.log(numberList);
+} while(sum < 50);
+
+// stampo in console
+console.log(sum);
