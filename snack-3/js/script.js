5d149866a0d07b398d7219db3c759ce8d20c9dae
chiaraxs <chiaracardinale92@gmail.com>
2021-12-16 10:18:40 +0100

    snack-3 fixed

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -2,23 +2,11 @@
 // Stampa le potenze di 2 fino a 1000.
 
 
-// ordine di costruzione:
-// Fammi un ciclo while che mi stampa i primi 10 cubi per i numeri da 1 a 10
-// Adesso cambiamo la condizione: fammi un ciclo while che mi stampa finché il valore del cubo è minore di 1000 
+let i = 2;
+let y = 2;
 
-let i = 0;
-let y = 2
-
-// while (i < 10) {
-//     console.log(Math.pow(i,3));
-//     i++;
-// }
-
-while (y < 1000) {
+while (Math.pow (2,i) < 1000) {
     y = Math.pow(2,i);
-    i++;
-    if (y >= 1000) {
-        break;
-    }
     console.log(y);
+    i++;
 }
